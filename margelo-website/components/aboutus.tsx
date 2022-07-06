633b5acf8338ec3a948687b3e765d8dfb5969282
Abdullah Atrash <abodiatrash@Abdullahs-MacBook-Air.local>
2022-07-07 01:11:02 +0200

    added the about us page

---
+++ margelo-website/components/aboutus.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const AboutUs: React.FC = () => {
+
+    return <section className={`flex flex-col bg-white py-20 text-3xl md:text-4xl`}>
+            <div className="container mx-auto px-11">
+                <p className="leading-tight max-w-5xl mx-auto text-4xl lg:text-4xl tracking-tight">
+                    <strong>we will help you ship better apps, faster </strong>
+                    Our team of expert engineers has created the best user experiences in some of most popular apps world wide
+                </p>
+            </div>
+           </section>
+
+}
+
+export default AboutUs
